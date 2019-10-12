8c3d9c89a38c60e127fe3e8a32da7dfda960d8c6
fdar0536 <32u.9j43@gmail.com>
2019-10-12 17:40:10 +0800

    update

--- src/common/basecommon.hpp
+++ src/common/basecommon.hpp
@@ -1,14 +1,13 @@
 #ifndef BASECOMMON_HPP
 #define BASECOMMON_HPP
 
-#ifdef _WIN32
+#if defined _WIN32 || defined __CYGWIN__
 #ifdef __MINGW32__
-#define SYMBOL_SHOW __attribute__((visibility("default")))
-#define SYMBOL_HIDE __attribute__((visibility("hidden")))
+#define SYMBOL_SHOW __attribute__ ((dllexport))
 #else
-#define SYMBOL_SHOW
-#define SYMBOL_HIDE
+#define SYMBOL_SHOW __declspec(dllexport)
 #endif // __MINGW32__
+#define SYMBOL_HIDE
 #else
 #define SYMBOL_SHOW __attribute__((visibility("default")))
 #define SYMBOL_HIDE __attribute__((visibility("hidden")))
