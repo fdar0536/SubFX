ba1255ca8c4cc5dbacc4be9863fcab6d88312654
fdar0536 <32u.9j43@gmail.com>
2019-12-07 21:16:24 +0800

    more sse and avx

--- src/YutilsCpp/math.cpp
+++ src/YutilsCpp/math.cpp
@@ -491,7 +491,107 @@ std::tuple<double, double, double> Math::bezier4(double pct,
                          std::vector<std::tuple<double, double, double>> &pts,
                                                  bool is3D)
 {
-    double pts1(0.f), pts2(0.f), pts3(0.f), pct_inv(1 - pct);
+    double pct_inv(1 - pct);
+#if defined(ENABLE_SIMD) && !defined(ENABLE_AVX) // SSE
+    double pctArray[] = {pct, pct};
+    double pct_inv_array[] = {pct_inv, pct_inv};
+    double threeArray[] = {3., 3.};
+
+    double ctrl0[] = {std::get<0>(pts.at(0)), std::get<1>(pts.at(0))};
+    double ctrl1[] = {std::get<0>(pts.at(1)), std::get<1>(pts.at(1))};
+    double ctrl2[] = {std::get<0>(pts.at(2)), std::get<1>(pts.at(2))};
+    double ctrl3[] = {std::get<0>(pts.at(3)), std::get<1>(pts.at(3))};
+
+    __m128d pctArray_reg(_mm_loadu_pd(pctArray));
+    __m128d pct_inv_array_reg(_mm_loadu_pd(pct_inv_array));
+    __m128d threeArray_reg(_mm_loadu_pd(threeArray));
+    __m128d ctrl0_reg(_mm_loadu_pd(ctrl0));
+    __m128d ctrl1_reg(_mm_loadu_pd(ctrl1));
+    __m128d ctrl2_reg(_mm_loadu_pd(ctrl2));
+    __m128d ctrl3_reg(_mm_loadu_pd(ctrl3));
+
+    __m128d pct2Array_reg(_mm_mul_pd(pctArray_reg, pctArray_reg));
+    __m128d pct_inv2_array_reg(_mm_mul_pd(pct_inv_array_reg, pct_inv_array_reg));
+
+    __m128d res_reg(_mm_mul_pd(pct_inv2_array_reg, pct_inv_array_reg));
+    res_reg = _mm_mul_pd(res_reg, ctrl0_reg);
+
+    __m128d tmp_reg(_mm_mul_pd(threeArray_reg, pct_inv2_array_reg));
+    tmp_reg = _mm_mul_pd(tmp_reg, pctArray_reg);
+    tmp_reg = _mm_mul_pd(tmp_reg, ctrl1_reg);
+    res_reg = _mm_add_pd(res_reg, tmp_reg);
+
+    tmp_reg = _mm_mul_pd(threeArray_reg, pct_inv_array_reg);
+    tmp_reg = _mm_mul_pd(tmp_reg, pct2Array_reg);
+    tmp_reg = _mm_mul_pd(tmp_reg, ctrl2_reg);
+    res_reg = _mm_add_pd(res_reg, tmp_reg);
+
+    tmp_reg = _mm_mul_pd(pct2Array_reg, pctArray_reg);
+    tmp_reg = _mm_mul_pd(tmp_reg, ctrl3_reg);
+    res_reg = _mm_add_pd(res_reg, tmp_reg);
+    _mm_storeu_pd(ctrl0, res_reg);
+
+    if (is3D)
+    {
+        double pts3 = pct_inv * pct_inv * pct_inv * std::get<2>(pts.at(0));
+        pts3 += (3. * pct_inv * pct_inv * pct * std::get<2>(pts.at(1)));
+        pts3 += (3. * pct_inv * pct * pct * std::get<2>(pts.at(2)));
+        pts3 += (pct * pct * pct * std::get<2>(pts.at(3)));
+        return std::make_tuple(ctrl0[0], ctrl0[1], pts3);
+    }
+
+    return std::make_tuple(ctrl0[0], ctrl0[1], 0.);
+#elif defined(ENABLE_SIMD) && defined(ENABLE_AVX) // AVX
+    double pctArray[] = {pct, pct, pct, pct};
+    double pct_inv_array[] = {pct_inv, pct_inv, pct_inv, pct_inv};
+    double threeArray[] = {3., 3., 3., 3.};
+
+    double ctrl0[] = {std::get<0>(pts.at(0)), std::get<1>(pts.at(0)),
+                      std::get<2>(pts.at(0)), 0};
+    double ctrl1[] = {std::get<0>(pts.at(1)), std::get<1>(pts.at(1)),
+                      std::get<2>(pts.at(1)), 0};
+    double ctrl2[] = {std::get<0>(pts.at(2)), std::get<1>(pts.at(2)),
+                      std::get<2>(pts.at(2)), 0};
+    double ctrl3[] = {std::get<0>(pts.at(3)), std::get<1>(pts.at(3)),
+                      std::get<2>(pts.at(3)), 0};
+
+    __m256d pctArray_reg(_mm256_loadu_pd(pctArray));
+    __m256d pct_inv_array_reg(_mm256_loadu_pd(pct_inv_array));
+    __m256d threeArray_reg(_mm256_loadu_pd(threeArray));
+    __m256d ctrl0_reg(_mm256_loadu_pd(ctrl0));
+    __m256d ctrl1_reg(_mm256_loadu_pd(ctrl1));
+    __m256d ctrl2_reg(_mm256_loadu_pd(ctrl2));
+    __m256d ctrl3_reg(_mm256_loadu_pd(ctrl3));
+
+    __m256d pct2Array_reg(_mm256_mul_pd(pctArray_reg, pctArray_reg));
+    __m256d pct_inv2_array_reg(_mm256_mul_pd(pct_inv_array_reg, pct_inv_array_reg));
+
+    __m256d res_reg(_mm256_mul_pd(pct_inv2_array_reg, pct_inv_array_reg));
+    res_reg = _mm256_mul_pd(res_reg, ctrl0_reg);
+
+    __m256d tmp_reg(_mm256_mul_pd(threeArray_reg, pct_inv2_array_reg));
+    tmp_reg = _mm256_mul_pd(tmp_reg, pctArray_reg);
+    tmp_reg = _mm256_mul_pd(tmp_reg, ctrl1_reg);
+    res_reg = _mm256_add_pd(res_reg, tmp_reg);
+
+    tmp_reg = _mm256_mul_pd(threeArray_reg, pct_inv_array_reg);
+    tmp_reg = _mm256_mul_pd(tmp_reg, pct2Array_reg);
+    tmp_reg = _mm256_mul_pd(tmp_reg, ctrl2_reg);
+    res_reg = _mm256_add_pd(res_reg, tmp_reg);
+
+    tmp_reg = _mm256_mul_pd(pct2Array_reg, pctArray_reg);
+    tmp_reg = _mm256_mul_pd(tmp_reg, ctrl3_reg);
+    res_reg = _mm256_add_pd(res_reg, tmp_reg);
+    _mm256_storeu_pd(ctrl0, res_reg);
+
+    if (is3D)
+    {
+        return std::make_tuple(ctrl0[0], ctrl0[1], ctrl0[2]);
+    }
+
+    return std::make_tuple(ctrl0[0], ctrl0[1], 0.);
+#else
+    double pts1(0.f), pts2(0.f), pts3(0.f);
 
     pts1 = pct_inv * pct_inv * pct_inv * std::get<0>(pts.at(0));
     pts1 += (3.f * pct_inv * pct_inv * pct * std::get<0>(pts.at(1)));
@@ -506,12 +606,13 @@ std::tuple<double, double, double> Math::bezier4(double pct,
     if (is3D)
     {
         pts3 = pct_inv * pct_inv * pct_inv * std::get<2>(pts.at(0));
-        pts3 += (3.f * pct_inv * pct_inv * pct * std::get<2>(pts.at(1)));
-        pts3 += (3.f * pct_inv * pct * pct * std::get<2>(pts.at(2)));
+        pts3 += (3. * pct_inv * pct_inv * pct * std::get<2>(pts.at(1)));
+        pts3 += (3. * pct_inv * pct * pct * std::get<2>(pts.at(2)));
         pts3 += (pct * pct * pct * std::get<2>(pts.at(3)));
     }
 
     return std::make_tuple(pts1, pts2, pts3);
+#endif
 }
 
 std::tuple<double, double, double> Math::bezierN(double pct,
