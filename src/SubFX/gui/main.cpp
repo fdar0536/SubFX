7c5fd88bd52709aee17e9bd0cf0cc84f669d697f
fdar0536 <32u.9j43@gmail.com>
2020-01-23 00:29:09 +0800

    update

--- src/SubFX/gui/main.cpp
+++ src/SubFX/gui/main.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 
+#include "pybind11/embed.h"
 #include "QApplication"
 
 #include "ui/mainwindow.hpp"
 
+namespace py = pybind11;
+
 int main(int argc, char **argv)
 {
     QApplication a(argc, argv);
+    py::scoped_interpreter guard{};
     MainWindow *w(nullptr);
     const char *err(nullptr);
     std::tie(w, err) = MainWindow::create(nullptr);
