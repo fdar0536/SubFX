7908661ede2ee9fd17fe69428842f86ae246ef50
fdar0536 <32u.9j43@gmail.com>
2020-02-15 00:00:31 +0800

    clean up utf8.hpp

--- src/YutilsCpp/utf8.hpp
+++ src/YutilsCpp/utf8.hpp
@@ -13,12 +13,14 @@ namespace Yutils
 class Utf8
 {
 protected:
+
     Utf8() {}
 
     uint32_t utf8StringLen(std::string&);
 
     // https://stackoverflow.com/questions/40054732/c-iterate-utf-8-string-with-mixed-length-of-characters
     std::vector<std::string> utf8StringSplit(std::string&);
+
 };
 
 }
