7f568a5e3169bc3a09c5c6eec8fb4884bc454b7a
fdar0536 <32u.9j43@gmail.com>
2020-02-15 00:02:32 +0800

    clean up YutilsCpp.hpp

--- src/YutilsCpp/YutilsCpp.hpp
+++ src/YutilsCpp/YutilsCpp.hpp
@@ -1,14 +1,15 @@
-#ifndef YUTILSCPP_HPP
-#define YUTILSCPP_HPP
+#pragma once
 
-/* assparser.hpp contains ass.hpp and utf8.hpp */
+#include "math.hpp"
+
+/*
+assparser.hpp contains ass.hpp and utf8.hpp
 #include "assparser.hpp"
 
-/* fonthandle.hpp contains math.hpp and common.hpp */
+fonthandle.hpp contains math.hpp and common.hpp
 #include "fonthandle.hpp"
 
 #include "file.hpp"
 
 #include "shape.hpp"
-
-#endif // YUTILSCPP_HPP
+*/
