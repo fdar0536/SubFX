43d7718e7955d6ce914ba975cf0bc623687d3aa2
fdar0536 <32u.9j43@gmail.com>
2020-07-26 17:04:55 +0800

    update

--- src/YutilsCpp/math.cpp
+++ src/YutilsCpp/math.cpp
@@ -1,3 +1,22 @@
+/*
+*    This file is part of SubFX,
+*    Copyright(C) 2019-2020 fdar0536.
+*
+*    SubFX is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU Lesser General Public License as
+*    published by the Free Software Foundation, either version 2.1
+*    of the License, or (at your option) any later version.
+*
+*    SubFX is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+*    GNU Lesser General Public License for more details.
+*
+*    You should have received a copy of the GNU Lesser General
+*    Public License along with SubFX. If not, see
+*    <http://www.gnu.org/licenses/>.
+*/
+
 #include <iostream>
 #include <algorithm>
 #include <limits>
@@ -315,5 +334,3 @@ Math::rotate(std::tuple<double, double, double> p,
                            cos(ra) * std::get<1>(p),
                            std::get<2>(p));
 }
-
-// private member functions
--- src/YutilsCpp/python/asspy.py
+++ src/YutilsCpp/python/asspy.py
@@ -1,6 +1,17 @@
-# This file is based on tcaxPy
-# Copyright (c) 2008 - 2012 milkyjing (milkyjing@gmail.com).
-# Copyright (c) 2020 fdar0536
+# This file is based on tcaxPy.
+# Copyright (c) 2008-2012 milkyjing (milkyjing@gmail.com).
+# Copyright (c) 2019-2020 fdar0536
+#
+# SubFX is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# SubFX is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with SubFX. If not, see <http://www.gnu.org/licenses/>.
 
 ######################################### Modules ###########################################################
 
