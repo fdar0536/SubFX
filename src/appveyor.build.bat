f24154668557a3a7eb6164811a7b9dc55e165837
fdar0536 <32u.9j43@gmail.com>
2020-08-21 18:54:02 +0800

    update build scripts

--- src/appveyor.build.bat
+++ src/appveyor.build.bat
@@ -1,42 +1,41 @@
-rem get vcpkg
-cd ../..
-git clone https://github.com/microsoft/vcpkg.git
-
-cd vcpkg
+rem update vcpkg
+cd /D c:\Tools\vcpkg
+git pull
 call bootstrap-vcpkg.bat
+vcpkg integrate install
 
-rem install libraries
-vcpkg install ^
-    boost-regex[icu]:x64-windows boost-locale[icu]:x64-windows ^
-    python3[enable-shared]:x64-windows python3:x64-windows ^
-    pybind11:x64-windows nlohmann-json:x64-windows ^
-    cxxopts:x64-windows
+rem update vcpkg packages
+vcpkg upgrade --no-dry-run
 
-cd ..
+vcpkg install boost-regex[icu]:x64-windows boost-locale[icu]:x64-windows ^
+    pybind11:x64 nlohmann-json:x64-windows cxxopts:x64-windows
+
+rem start build project
+cd /
+cd projects/SubFX/src
 mkdir build-windows
 cd build-windows
-"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -latest -property installationPath > tmp.txt
-set /p VCPATH=<tmp.txt
-call "%VCPATH%"/VC/Auxiliary/Build/vcvars64.bat
-cmake -DCMAKE_TOOLCHAIN_FILE=../vcpkg/scripts/buildsystems/vcpkg.cmake ^
-    -G "Visual Studio 16 2019" -DCMAKE_INSTALL_PREFIX="SubFX-Release" ..
+cmake -DCMAKE_TOOLCHAIN_FILE=c:/Tools/vcpkg/scripts/buildsystems/vcpkg.cmake ^
+    -G "Visual Studio 16 2019" -DPython_ROOT_DIR=C:/Python38-x64 ^
+    -DCMAKE_INSTALL_PREFIX="SubFX-Release" ..
 msbuild SubFX.sln /p:Configuration=Release /target:ALL_BUILD
 msbuild INSTALL.vcxproj /p:Configuration=Release
+
 cd SubFX-Release
 copy "..\..\build\windows\install.bat" "install.bat"
 copy "..\..\build\windows\uninstall.bat" "uninstall.bat"
 
 cd bin
 rem copy boost's dll
-copy "..\..\..\vcpkg\installed\x64-windows\bin\boost_regex-vc142-mt-x64-1_73.dll" ^
+copy "c:\Tools\vcpkg\installed\x64-windows\bin\boost_regex-vc142-mt-x64-1_73.dll" ^
     "boost_regex-vc142-mt-x64-1_73.dll"
 
 rem copy icu's dll
-copy "..\..\..\vcpkg\installed\x64-windows\bin\icuin67.dll" ^
+copy "c:\Tools\vcpkg\installed\x64-windows\bin\icuin67.dll" ^
     "icuin67.dll"
-copy "..\..\..\vcpkg\installed\x64-windows\bin\icuuc67.dll" ^
+copy "c:\Tools\vcpkg\installed\x64-windows\bin\icuuc67.dll" ^
     "icuuc67.dll"
-copy "..\..\..\vcpkg\installed\x64-windows\bin\icudt67.dll" ^
+copy "c:\Tools\vcpkg\installed\x64-windows\bin\icudt67.dll" ^
     "icudt67.dll"
 
 rem prepare for testing
@@ -52,9 +51,7 @@ copy "..\in.ass" "in.ass"
 cd ../../../../
 msbuild RUN_TESTS.vcxproj /p:Configuration=Release
 if not errorlevel 0 (
-    pause
     exit /b 1
 )
 
 7z.exe a -t7z SubFX-Release.7z SubFX-Release
-pause
